72f6a8af7006329f51f4fcb8af5fc8eb992f1ccf
Katzzz4 <rifqirmdnsyah@gmail.com>
2026-04-05 14:47:10 +0700

    update desain admin panel + component layout, dashboard, dan sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,13 @@
+<aside class="w-64 bg-gray-800 text-white flex flex-col">
+    <div class="p-4 text-2xl font-bold border-b border-gray-700">
+        Admin Panel
+    </div>
+    <nav class="flex-1 p-4 space-y-2">
+        <a href="/dashboard" class="block p-2 rounded hover:bg-gray-700">Dashboard</a>
+        <a href="#" class="block p-2 rounded hover:bg-gray-700">Data Produk</a>
+        <a href="#" class="block p-2 rounded hover:bg-gray-700">Transaksi</a>
+        <a href="#" class="block p-2 rounded hover:bg-gray-700">Stok Barang</a>
+        <a href="#" class="block p-2 rounded hover:bg-gray-700">Laporan</a>
+        <a href="#" class="block p-2 rounded hover:bg-gray-700">Manajemen User</a>
+    </nav>
+</aside>
